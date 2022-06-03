2ec8580ed40850c643b471607ad00561a386beb4
Kevin <11414927+kcheilman@users.noreply.github.com>
2022-06-03 09:44:49 -0700

    Include the emacs customization information file

---
+++ .emacs.d/custom_efs.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;;'(custom-enabled-themes '(tsdh-dark))
+ '(package-selected-packages
+   '(highlight-indent-guides dired-open all-the-icons-dired dired-single evil-nerd-commenter forge company-box company which-key doom-modeline all-the-icons doom-themes use-package typescript-mode rainbow-delimiters pyvenv python-mode no-littering magit ivy-rich ivy-prescient eterm-256color counsel-projectile auto-package-update)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mode-line-inactive ((t (:background "#303d45" :foreground "#339CDB" :box nil))))
+ )
